e39322588faf9de266c400b9ca5204bce52244cc
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-09-23 00:15:42 +0530

    Create class and object.cpp

---
+++ class and object.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class hero {
+    
+    //this is a propertie of class 
+    int health;
+    char level;
+   
+    
+};
+
+int main()
+{
+    // this is a object
+    hero h1;
+ 
+    cout<<"size "<<sizeof(h1)<<endl;
+}
